4fd0450e30a16a584ff8577194ecde3d048524f1
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-03-18 19:36:06 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -12,6 +12,46 @@ int main()
     cin >> num1 >> num2 >> num3;
 
     // TODO: Find min, max, and med among num1, num2, num3
-
+    if (num1 < num2){
+        if (num1 < num3){
+            if (num3 < num2){
+                min = num1;
+                med = num3;
+                max = num2;
+            }
+            else{
+            min = num1;
+            med = num2;
+            max = num3;
+            }
+        }
+        else{
+            min = num3;
+            med = num1;
+            max = num2;
+        }
+    }
+    else{
+        if (num2 < num3){
+            if (num1 < num3){
+            min = num2;
+            med = num1;
+            max = num3;
+            }
+            else{
+            min = num2;
+            med = num3;
+            max = num1;
+            }
+        }
+        else{
+            min = num3;
+            med = num2;
+            max = num1;
+        }
+    }
     cout << min << " " << med << " " << max << endl;
 }
+
+
+
